84f0e0d38e0cac46552b4b99ad7724447580a031
udaymondal <udaysk.mondal@gmail.com>
2021-02-24 00:07:33 +0600

    first app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit done <code>src/App.js</code> and save to reload & pushed to github.
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React :: Started
         </a>
       </header>
     </div>
