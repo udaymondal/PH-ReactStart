16533b5d76748c0ef47dd1aa6d761966e2f7fa15
udaymondal <udaysk.mondal@gmail.com>
2021-02-26 21:09:08 +0600

    startAgain

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var person= {
-    name: "Kudus",
-    age: 20,
-  }
-  var singer = {
-    fullName: "Hero Alam",
-    age: 30,
-    medium: "facebook"
-  }
-  var actor = {
-    movieName: "Jolil Bhai",
-    age: 40,
+  // var person= {
+  //   name: "Kudus",
+  //   age: 20,
+  // }
+  // var singer = {
+  //   fullName: "Hero Alam",
+  //   age: 30,
+  //   medium: "facebook"
+  // }
+  // var actor = {
+  //   movieName: "Jolil Bhai",
+  //   age: 40,
 
-  }
+  // }
 
-  var styleAnotherWay = {
-    backgroundColor: 'goldenrod'
-  }
+  // var styleAnotherWay = {
+  //   backgroundColor: 'goldenrod'
+  // }
+  const nayoks = ['Manna', 'Illias', 'Salman']
   return (
     <div className="App">
       <header className="App-header">
@@ -30,18 +31,20 @@ function App() {
         {/* <p>singer for to day {singer}</p> */}
 
         <h3>VVIP BD</h3>
-        <h5 className="actorColor">{actor.movieName} is having jhamela with {singer.fullName}</h5>
-        <h5 style={styleAnotherWay}>One is {singer.age} years old and one is {person.age+actor.age} years old </h5>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        {/* <h5 className="actorColor">{actor.movieName} is having jhamela with {singer.fullName}</h5>
+        <h5 style={styleAnotherWay}>One is {singer.age} years old and one is {person.age+actor.age} years old </h5> */}
+        <Person name="Jolil Bhai" nayika="Barsha"></Person>
+        <Person name="Scakib" nayika="Shobai"></Person>
+        <Person name="BappaRazz" nayika=""></Person>
+        <Person name="Misha Shoudagor" nayika="Maal"></Person>
+        <Person name={nayoks[1]}></Person>
       </header>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
+  console.log(props);
   return (
     <div style={{
       backgroundColor: "green",
@@ -49,8 +52,8 @@ function Person(){
       margin: "10px",
       padding: "10px"
       }}>
-      <h3>Name: Called from another function</h3>
-      <h5>Person is called</h5>
+      <h3>Name: {props.name}</h3>
+      <h5>Hero of {props.nayika}</h5>
     </div>
   )
 }
