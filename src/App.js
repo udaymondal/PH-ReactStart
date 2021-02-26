6c595ea30a494b8e68e784a9a1f8abcd822a09a3
udaymondal <udaysk.mondal@gmail.com>
2021-02-26 11:26:33 +0600

    returned

--- src/App.js
+++ src/App.js
@@ -16,21 +16,30 @@ function App() {
     age: 40,
 
   }
+
+  var styleAnotherWay = {
+    backgroundColor: 'goldenrod'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>My heading</h1>
+        <h1 style={{backgroundColor: "blue"}}>My heading</h1>
         <h5>testing cal:  {2+6} </h5>
         <p>My first react paragraph</p><br/>
         {/* <p>singer for to day {singer}</p> */}
 
         <h3>VVIP BD</h3>
-        <h5>{actor.movieName} is having jhamela with {singer.fullName}</h5>
-        <h5>One is {actor.age} years old and one is {person.age+actor.age} years old </h5>
+        <h5 className="actorColor">{actor.movieName} is having jhamela with {singer.fullName}</h5>
+        <h5 style={styleAnotherWay}>One is {singer.age} years old and one is {person.age+actor.age} years old </h5>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  return <h1 style={{background: "tomato"}}>Name: Called from another function</h1>
 }
 
 export default App;
